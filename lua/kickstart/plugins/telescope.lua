72f475c6cbd845bd25e348137febd394db38c3bd
valenjeb <vala.jebelev@gmail.com>
2024-04-26 12:19:06 +0300

    Ignore node_modules dir from telescope search

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -56,11 +56,12 @@ return {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          --   mappings = {
+          --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          --   },
+          file_ignore_patterns = { 'node_modules' },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
